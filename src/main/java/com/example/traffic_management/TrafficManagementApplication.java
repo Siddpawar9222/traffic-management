dd68e8ffe393750ec2d35bbb5c10e260bc207039
siddhesh <siddpawar583@gmail.com>
2025-08-19 19:23:44 +0530

    feature test added

--- src/main/java/com/example/traffic_management/TrafficManagementApplication.java
+++ src/main/java/com/example/traffic_management/TrafficManagementApplication.java
@@ -3,6 +3,8 @@ package com.example.traffic_management;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+// Changes by feature/test
 @SpringBootApplication
 public class TrafficManagementApplication {
 
